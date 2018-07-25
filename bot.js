67ef0f2bae88623f879e631d1ac46fafffd00e87
Ciarán Mc Aree <37655585+firegene@users.noreply.github.com>
2018-07-25 17:53:29 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,116 +1,53 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+let token = "token"
+let prefix = "stock."
+let password = "password"
 let timers = [];
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity(`with my sexy ${prefix} prefix`); 
 });
 
 client.on('message', msg => {
+	
+if(msg.content.startsWith("```purchase void```"))  {
+	msg.channel.send(".sell all");
+} 
 
-if(msg.content.startsWith("stock.pumpcoal")){
+if (msg.content.indexOf(prefix) !== 0) return;
+const args = msg.content.slice(prefix.length).trim().split(/ +/g);
+const command = args.shift().toLowerCase();
+
+
+
+if(command === "pump"){
+	if(args[1] === undefined) {args.push(2000)};
+	   console.log(`${msg.author.tag} started pumping ${args[0]}`);
+	   let y = parseInt(args[1]);
     let x = setInterval(function() {
-        msg.channel.send('.buy 1 coal');
-    }, 2000);
+        msg.channel.send(`.buy 1 ${args[0]}`);
+    }, y);
     timers.push(x);
 }
 
-else if(msg.content.startsWith("stock.pumpcopper")){
-    let x = setInterval(function() {
-        msg.channel.send('.buy 1 copper');
-    }, 2000);
-    timers.push(x);
+else if(command === "sell"){
+	      console.log(`${msg.author.tag} sold all`)
+        msg.channel.send('.sell all')
 }
 
-else if(msg.content.startsWith("stock.pumpoil")){
-    let x = setInterval(function() {
-        msg.channel.send('.buy 1 oil');
-    }, 2000);
-    timers.push(x);
-}
-
-else if(msg.content.startsWith("stock.pumpsilver")){
-    let x = setInterval(function() {
-        msg.channel.send('.buy 1 silver');
-    }, 2000);
-    timers.push(x);
-}
-
-else if(msg.content.startsWith("stock.pumpgold")){
-    let x = setInterval(function() {
-        msg.channel.send('.buy 1 gold');
-    }, 2000);
-    timers.push(x);
-}
-
-else if(msg.content.startsWith("stock.pumpcotton")){
-    let x = setInterval(function() {
-        msg.channel.send('.buy 1 cotton');
-    }, 2000);
-    timers.push(x);
-}
-
-else if(msg.content.startsWith("stock.pumpwheat")){
-    let x = setInterval(function() {
-        msg.channel.send('.buy 1 wheat');
-    }, 2000);
-    timers.push(x);
-}
-
-else if(msg.content.startsWith("stock.pumpcattle")){
-    let x = setInterval(function() {
-        msg.channel.send('.buy 1 cattle');
-    }, 2000);
-    timers.push(x);
-}
-
-else if(msg.content.startsWith("stock.pumpplatinum")){
-    let x = setInterval(function() {
-        msg.channel.send('.buy 1 platinum');
-    }, 2000);
-    timers.push(x);
-}
-
-else if(msg.content.startsWith("stock.pumpcocoa")){
-    let x = setInterval(function() {
-        msg.channel.send('.buy 1 cocoa');
-    }, 2000);
-    timers.push(x);
-}
-
-else if(msg.content.startsWith("stock.pumpcorn")){
-    let x = setInterval(function() {
-        msg.channel.send('.buy 1 corn');
-    }, 2000);
-    timers.push(x);
-}
-
-else if(msg.content.startsWith("stock.pumpsugar")){
-    let x = setInterval(function() {
-        msg.channel.send('.buy 1 sugar');
-    }, 2000);
-    timers.push(x);
-}
-
-else if(msg.content.startsWith("stock.pumpuranium")){
-    let x = setInterval(function() {
-        msg.channel.send('.buy 1 uranium');
-    }, 2000);
-    timers.push(x);
-}
-
-else if(msg.content.startsWith("stock.lotto")){
+else if(command === "lotto"){
+	      console.log(`${msg.author.tag} bought a lottery ticket`) 
         msg.channel.send('.lotto')
 }
 
-else if(msg.content.startsWith("```purchase void```"))  {
-	msg.channel.send(".sell all");
-	} 
-
-else if(msg.content.startsWith("stock.stop password")){
+else 
+	
+if(command === `stop` && args[0] === password){
    timers.forEach(timer => clearInterval(timer));
    console.log(`Bot stopped by ${msg.author.tag}`);
 }
 });
-
-client.login('token');
+ 
+client.login(token);
